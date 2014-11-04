829345e07a5cc6ab7d4034682dc272f4ff92f855
VentsiKonov <ventsi.konov@gmail.com>
2014-11-04 17:03:54 +0200

    List almost done

--- DSA-Homework/Supermarket/List.h
+++ DSA-Homework/Supermarket/List.h
@@ -1,26 +1,30 @@
 #pragma once
-
+#include <assert.h>
 template <class T>
 class List {
 public:
 	List();
 	~List();
-	List& operator=(const List<T>& l);
+	List<T>& operator=(const List<T>& l);
 	List(const List<T>& l);
 
 	size_t Size() const;
 	void PushFront(const T& data);
 	void PushBack(const T& data);
+	void PushAt(size_t index, const T& data);
 	T PopFront();
 	T PopBack();
-	T PeakFront();
-	T PeakBack();
+	T PopAt(size_t index);
+	T PeakFront() const;
+	T PeakBack() const;
+	T PeakAt(size_t index);
+	void Clear();
 
 protected:
 	class Node {
+	public:
 		T data;
 		Node* next;
-	public:
 		Node();
 		Node(const T& data, Node* next);
 	private:
@@ -54,11 +58,118 @@ List<T>::List() {
 
 template <class T>
 List<T>::~List() {
+	Clear();
+}
+
+template <class T>
+void List<T>::CopyFrom(const List<T>& l) {
+	Node* it = l.first;
+	if (!it) return;
+	do {
+		this->PushBack(it->data);
+		it = it->next;
+	} while (it);
+}
+
+template <class T>
+void List<T>::Clear() {
 	Node* current;
 	while (size--) {
 		current = first;
 		first = first->next;
 		delete current;
 	}
+	size = 0;
+	last = nullptr;
 }
 
+template <class T>
+List<T>::List(const List<T>& l) {
+	if (this != &l) {
+		first = last = nullptr;
+		size = 0;
+		this->CopyFrom(l);
+	}
+}
+
+template <class T>
+List<T>& List<T>::operator=(const List<T>& l) {
+	if (this != &l) {
+		this->Clear();
+		this->CopyFrom(l);
+	}
+
+	return *this;
+}
+
+template <class T>
+void List<T>::PushBack(const T& data) {
+	if (!last) 
+		return PushFront(data);
+
+	last = last->next = new Node(data, nullptr);
+	++size;
+}
+
+template <class T>
+void List<T>::PushFront(const T& data) {
+	first = new Node(data, first);
+	if (!last) last = first;
+	++size;
+}
+
+template <class T>
+T List<T>::PeakFront() const {
+	return first->data;
+}
+
+template <class T>
+T List<T>::PeakBack() const {
+	return last->data;
+}
+
+template <class T>
+size_t List<T>::Size() const {
+	return size;
+}
+
+template <class T>
+T List<T>::PopFront() {
+	T returnValue = PeakFront();
+	Node* current = first;
+	first = first->next;
+	--size;
+	delete current;
+	return returnValue;
+}
+
+template <class T>
+T List<T>::PopBack() {
+	T returnValue = PeakBack();
+	Node* it = first; 
+	while (it->next != last) {
+		it = it->next;
+	}
+	last = it;
+	--size;
+	return returnValue;
+}
+
+template <class T>
+T List<T>::PeakAt(size_t index) {
+	Node* it = first;
+	while (index--) {
+		it = it->next;
+	}
+	return it->data;
+}
+
+template <class T>
+T List<T>::PopAt(size_t index) {
+
+}
+
+template <class T>
+void List<T>::PushAt(size_t index) {
+
+}
--- DSA-Homework/Supermarket/Main.cpp
+++ DSA-Homework/Supermarket/Main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include "List.h"
+using namespace std;
 int main() {
+	{
+		List<int> a;
+		a.PushFront(5);
+		a.PushBack(10);
+		std::cout << a.PeakFront() << " " << a.PeakBack();
+		List<int> b(a);
+		a.Clear();
+		std::cout << b.PeakAt(1) << " " << b.PopBack();
+	}
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
