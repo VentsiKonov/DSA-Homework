cb1bf71bc1e0ba40bb38aa0eccd091234253c6a3
VentsiKonov <ventsi.konov@gmail.com>
2014-11-06 22:38:37 +0200

    Added task-required interfaces

---
+++ DSA-Homework/Supermarket/Market.h
@@ -0,0 +1,27 @@
+#pragma once
+
+struct ClientState {
+	int CashDeskPosition; // номер на каса
+	int QueuePosition; // позиция в опашката на касата
+	Client * client;
+};
+
+struct MarketState {
+	int numberOfCashDesk; // броя на касите които са отворили в момента
+	int * numberOfClientsAtCashDecsk; // броя на клиентите на всяка каса в този момент
+	int numberOfClientsAtExpressCashDeck;
+};
+
+struct Client {
+	int ID; // уникален номер на клиента в магазина
+	int numberOfGoods; // брой на покупките на клиента
+	bool creditCard; // истина ако плаща с крединта карта
+};
+
+class Market {
+public:
+	Market(int NumberOfAllCashDecks); // максимални брой каси които може да бъдат отворени в магазина (без експресната)
+	void AddClient(Client * clients, int number); // добавяме number клиенти в магазина
+	MarketState getMarketState(); // връща състоянието на магазина
+	ClientState getClientState(int ID); // връща състоянието на клиента
+};
