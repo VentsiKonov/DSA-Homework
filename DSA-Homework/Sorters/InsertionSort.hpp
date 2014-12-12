0fd66e7cf74b6f171fb81df09406abc7ca584425
VentsiKonov <ventsi.konov@gmail.com>
2014-12-12 23:29:59 +0200

    QuickSort and InsertionSort tested

--- DSA-Homework/Sorters/InsertionSort.hpp
+++ DSA-Homework/Sorters/InsertionSort.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "Sorter.h"
-
+#include <ctime>
 template<typename T>
 class InsertionSort : Sorter<T> {
 public:
@@ -8,18 +8,28 @@ public:
 	virtual void sort(T * data, size_t count);
 
 	virtual unsigned long long getSortTime() const;
+
+private:
+	clock_t time;
 };
 
 template <class T>
 unsigned long long InsertionSort<T>::getSortTime() const {
-	return 0;
+	return time;
 }
 
 template <class T>
 void InsertionSort<T>::sort(T* data, size_t count) {
+	time = clock();
 	T inHand;
 	size_t index;
-	for (size_t i = 1; i < count - 1; i++) {
+	for (size_t i = 1; i < count; i++) {
+#ifdef _DEBUG
+		for (size_t j = 0; j < count; j++) {
+			std::cout << data[j] << " ";
+		}
+		std::cout << std::endl;
+#endif
 		inHand = data[i];
 		index = i;
 		while (index > 0 && inHand < data[index - 1]) {
@@ -28,4 +38,5 @@ void InsertionSort<T>::sort(T* data, size_t count) {
 		}
 		data[index] = inHand;
 	}
+	time = clock() - time;
 }
--- DSA-Homework/Sorters/Main.cpp
+++ DSA-Homework/Sorters/Main.cpp
@@ -1,25 +1,47 @@
 #include "InsertionSort.hpp"
+#include "QuickSort.hpp"
 #include <iostream>
 
-int main() {
-	
-	InsertionSort<int> sorter;
-	int* data;
-	data = new int[10];
-	for (size_t i = 0; i < 10; i++) {
-		data[i] = rand() % 15;
+bool isSorted(int* data, size_t count) {
+	for (size_t i = 1; i < count; ++i) {
+		if (data[i - 1] > data[i])
+			return false;
 	}
+	return true;
+}
 
-	for (size_t i = 0; i < 10; i++) {
+int main() {
+
+	QuickSort<int> quick;
+	InsertionSort<int> insertion;
+	int* data;
+	size_t count = 1620400;
+	data = new int[count];
+	for (size_t i = 0; i < count; i++) {
+		data[i] = rand() % 5000;
+	}
+#ifdef _DEBUG
+	for (size_t i = 0; i < count; i++) {
 		std::cout << data[i] << " ";
 	}
-
+	std::cout << std::endl;
+#endif
 	std::cout << "\n";
 
-	sorter.sort(data, 10);
+	// head
 
-	for (size_t i = 0; i < 10; i++) {
-		std::cout << data[i] << " ";
+	quick.sort(data, count);
+
+	// foot
+	bool sorted = isSorted(data, count);
+	std::cout << "Sorted: " << sorted << std::endl;
+	if (!sorted) {
+		for (size_t i = 0; i < count; i++) {
+			std::cout << data[i] << " ";
+		}
+		std::cout << std::endl;
 	}
+	std::cout << quick.getSortTime() << " ticks." << std::endl;
 	return 0;
 }
+
