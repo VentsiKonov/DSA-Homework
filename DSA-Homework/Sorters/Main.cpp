a75bb3810590d389bf683f0b7202aeae12e88637
VentsiKonov <ventsi.konov@gmail.com>
2014-12-13 14:39:58 +0200

    merge sort implemented

--- DSA-Homework/Sorters/Main.cpp
+++ DSA-Homework/Sorters/Main.cpp
@@ -1,6 +1,7 @@
 #include "InsertionSort.hpp"
 #include "QuickSort.hpp"
 #include "HeapSort.hpp"
+#include "MergeSort.hpp"
 
 #include <iostream>
 
@@ -17,12 +18,15 @@ int main() {
 	QuickSort<int> quick;
 	InsertionSort<int> insertion;
 	HeapSort<int> heap;
+	MergeSort<int> merge;
 
-	int* data;
-	size_t count = 10000000;
+	int *data, *data2, *data3;
+	size_t count = 0;
 	data = new int[count];
+	data2 = new int[count];
+	data3 = new int[count];
 	for (size_t i = 0; i < count; i++) {
-		data[i] = rand() % 5000;
+		data[i] = data2[i] = data3[i] = rand() % 5000;
 	}
 #ifdef _DEBUG
 	for (size_t i = 0; i < count; i++) {
@@ -34,18 +38,24 @@ int main() {
 
 	// head
 
-	heap.sort(data, count);
+	//heap.sort(data, count);
+	//std::cout << heap.getSortTime() << " ticks heap." << std::endl;
+	//quick.sort(data2, count); 
+	//std::cout << quick.getSortTime() << " ticks quick." << std::endl;
+	//insertion.sort(data3, count);
+	//std::cout << insertion.getSortTime() << " ticks insertion." << std::endl;
+
+	merge.sort(data, count);
 
 	// foot
 	bool sorted = isSorted(data, count);
 	std::cout << "Sorted: " << sorted << std::endl;
 	if (!sorted) {
-		for (size_t i = 0; i < count; i++) {
-			std::cout << data[i] << " ";
-		}
+		//for (size_t i = 0; i < count; i++) {
+		//	std::cout << data[i] << " ";
+		//}
 		std::cout << std::endl;
 	}
-	std::cout << heap.getSortTime() << " ticks." << std::endl;
 	return 0;
 }
 
---
+++ DSA-Homework/Sorters/MergeSort.hpp
@@ -0,0 +1,79 @@
+#include "Sorter.h"
+
+template <class T>
+class MergeSort : Sorter < T > {
+public:
+	virtual void sort(T* data, size_t count);
+	virtual unsigned long long getSortTime() const;
+
+private:
+	void merge(T* input, T* output, size_t leftStart, size_t rightStart, size_t rightEnd);
+	size_t min(size_t lhs, size_t rhs) {
+		return lhs < rhs ? lhs : rhs;
+	}
+
+	T* workArray;
+	time_t time;
+};
+
+template <class T>
+unsigned long long MergeSort<T>::getSortTime() const {
+	return time;
+}
+
+template <class T>
+void MergeSort<T>::merge(T* input, T* output, size_t leftStart, size_t rightStart, size_t rightEnd) {
+	
+	size_t left = leftStart;
+	size_t right = rightStart;
+	size_t writer = leftStart;
+
+	while (writer < rightEnd) {
+		if (right >= rightEnd || (left < rightStart && input[left] < input[right])) {
+			output[writer++] = input[left++];
+		}
+		else {
+			output[writer++] = input[right++];
+		}
+	}
+
+}
+
+template <class T>
+void MergeSort<T>::sort(T* data, size_t count) {
+	time = clock();
+	try {
+		workArray = new T[count];
+	}
+	catch (std::bad_alloc ex) {
+		std::cerr << "Merge sort failed! No enough space!";
+		return;
+	}
+
+	size_t runNumber = 0;
+	T *input, *output;
+	for (size_t size = 1; size < count; size *= 2) {
+		for (size_t i = 0; i < count; i += 2*size) {
+			if (runNumber % 2 == 0) {
+				input = data;
+				output = workArray;
+			}
+			else {
+				input = workArray;
+				output = data;
+			}
+			merge(input, output, i, min(i + size, count), min(i + 2 * size, count));
+		}
+		++runNumber;
+	}
+
+	if (runNumber % 2 != 0) {
+		for (size_t i = 0; i < count; i++) {
+			data[i] = workArray[i];
+		}
+	}
+
+	delete[] workArray;
+
+	time = time - clock();
+}
